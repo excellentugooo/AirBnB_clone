a01c603bc657967cfbb75fa88cb8cea52729d70b
Mimifresh <mimigolld@gmail.com>
2023-09-17 12:10:46 -0700

    debuged

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-__init__ dunder method for the models directory
+__init__ dunder method for the models directory..
 """
 from models.engine.file_storage import FileStorage
 
